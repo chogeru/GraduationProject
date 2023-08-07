56c6bcfff76014c4cf4efc9761708083a8030ea7
ABURAMUSI <108376848+chogeru@users.noreply.github.com>
2023-08-07 15:53:07 +0900

    assets

---
+++ OnlineProject/Assets/Blaze AI/Scripts/Behaviours/Editor/SurprisedStateBehaviourInspector.cs
@@ -0,0 +1,57 @@
+using UnityEditor;
+
+namespace BlazeAISpace 
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(SurprisedStateBehaviour))]
+    public class SurprisedStateBehaviourInspector : Editor
+    {
+        SerializedProperty anim,
+        animT,
+        duration,
+        playAudio,
+        onStateEnter,
+        onStateExit;
+
+
+        void OnEnable()
+        {
+            anim = serializedObject.FindProperty("anim");
+            animT = serializedObject.FindProperty("animT");
+            duration = serializedObject.FindProperty("duration");
+            playAudio = serializedObject.FindProperty("playAudio");
+            onStateEnter = serializedObject.FindProperty("onStateEnter");
+            onStateExit = serializedObject.FindProperty("onStateExit");
+        }
+
+
+        public override void OnInspectorGUI () 
+        {
+            SurprisedStateBehaviour script = (SurprisedStateBehaviour) target;
+            int spaceBetween = 10;
+
+            EditorGUILayout.LabelField("Hover on any property below for insights", EditorStyles.helpBox);
+            EditorGUILayout.Space(10);
+
+            EditorGUILayout.LabelField("ANIMATION", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(anim);
+            EditorGUILayout.PropertyField(animT);
+            EditorGUILayout.Space(spaceBetween);
+
+            EditorGUILayout.LabelField("DURATION", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(duration);
+            EditorGUILayout.Space(spaceBetween);
+
+            EditorGUILayout.LabelField("PLAY AUDIO", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(playAudio);
+            EditorGUILayout.Space(spaceBetween);
+
+            EditorGUILayout.LabelField("STATE EVENTS", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(onStateEnter);
+            EditorGUILayout.PropertyField(onStateExit);
+
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ OnlineProject/Assets/Blaze AI/Scripts/Behaviours/SurprisedStateBehaviour.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace BlazeAISpace
+{
+    [AddComponentMenu("Surprised State Behaviour/Surprised State Behaviour")]
+    public class SurprisedStateBehaviour : MonoBehaviour
+    {
+        [Tooltip("The surprised animation to play.")]
+        public string anim;
+        [Tooltip("The animation transition.")]
+        public float animT = 0.25f;
+        [Tooltip("The duration to stay in this state and playing the animation.")]
+        public float duration;
+
+        [Tooltip("Set your audios in the audio scriptable in the General Tab in Blaze AI.")]
+        public bool playAudio;
+
+        public UnityEvent onStateEnter;
+        public UnityEvent onStateExit;
+
+
+        #region BEHAVIOUR VARS
+
+        BlazeAI blaze;
+        float _duration = 0f;
+
+        bool playedAudio;
+        bool turningDone;
+
+        #endregion
+
+        #region UNITY METHODS
+        
+        void Start()
+        {
+            blaze = GetComponent<BlazeAI>();
+
+            // force shut if not the same state
+            if (blaze.state != BlazeAI.State.surprised) {
+                enabled = false;
+            }
+        }
+
+        void OnDisable()
+        {
+            Reset();
+            onStateExit.Invoke();
+        }
+
+        void OnEnable()
+        {
+            onStateEnter.Invoke();
+        }
+        
+        void Update()
+        {
+            // only turn if turning hasn't finished
+            if (!turningDone) 
+            {
+                // turn to face enemy -> this function returns true when done
+                if (blaze.TurnTo(blaze.enemyPosOnSurprised, blaze.waypoints.leftTurnAnimAlert, blaze.waypoints.rightTurnAnimAlert, blaze.waypoints.turningAnimT, 20)) {
+                    turningDone = true;
+                }
+
+                return;
+            }
+            
+            
+            // play animation
+            blaze.animManager.Play(anim, animT);
+            
+            
+            // play audio
+            if (playAudio && !playedAudio) 
+            {
+                if (!blaze.IsAudioScriptableEmpty()) {
+                    if (blaze.PlayAudio(blaze.audioScriptable.GetAudio(AudioScriptable.AudioType.SurprisedState))) {
+                        playedAudio = true;
+                    }
+                }
+            }
+            
+
+            // timer to quit surprised state
+            _duration += Time.deltaTime;
+            
+            if (_duration >= duration) 
+            {
+                Reset();
+                blaze.SetState(BlazeAI.State.attack);
+            }
+        }
+
+        void Reset()
+        {
+            turningDone = false;
+            _duration = 0f;
+            playedAudio = false;
+        }
+
+        #endregion
+    }
+}
+
