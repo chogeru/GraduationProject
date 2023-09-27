b0c54c338032a14c5380421ab3dc678f4491a67f
ABURAMUSI <108376848+chogeru@users.noreply.github.com>
2023-09-27 13:27:34 +0900

    機能修正

--- OnlineProject/Assets/AbubuResource/Scripts/Wepon/GunShot.cs
+++ OnlineProject/Assets/AbubuResource/Scripts/Wepon/GunShot.cs
@@ -1,30 +1,35 @@
+using MalbersAnimations.Controller;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GunShot : MonoBehaviour
 {
-    [SerializeField,Header("球のプレハブ")]
+    [SerializeField, Header("球のプレハブ")]
     private GameObject m_BulletPrefab;
     [SerializeField, Header("チャージショットの弾")]
     private GameObject m_ChargeBulletPrefab;
-    [SerializeField,Header("銃口オブジェクトのTransform")]
+    [SerializeField, Header("銃口オブジェクトのTransform")]
     private Transform m_MuzzleTransform;
-    [SerializeField,Header("弾速")]
+    [SerializeField, Header("弾速")]
     private float m_BulletSpeed = 10f;
     [SerializeField, Header("クールタイム")]
     private float m_CoolTime = 0.1f;
     private float m_Time;
-    
+    private bool isCharge=true;
+    [SerializeField]
+    private float m_ChargeTime = 1.5f;
+    [SerializeField]
+    private float m_ChargeCoolTime;
     [SerializeField, Header("発射SE")]
     private AudioClip m_AudioGunSE;
-    private float m_Volume=0.6f;
+    private float m_Volume = 0.6f;
     [SerializeField, Header("弾発射SE")]
     private GameObject m_ParticleGun;
     // 発射ボタンを処理するメソッド
     private void Fire()
     {
-        AudioSource.PlayClipAtPoint(m_AudioGunSE, transform.position,m_Volume);
+        AudioSource.PlayClipAtPoint(m_AudioGunSE, transform.position, m_Volume);
         // 球のプレハブから新しい球を生成
         GameObject bullet = Instantiate(m_BulletPrefab, m_MuzzleTransform.position, m_MuzzleTransform.rotation);
         GameObject particle = Instantiate(m_ParticleGun, m_MuzzleTransform.position, m_MuzzleTransform.rotation);
@@ -37,13 +42,47 @@ public class GunShot : MonoBehaviour
             bulletRigidbody.velocity = m_MuzzleTransform.forward * m_BulletSpeed;
         }
     }
+    private void ChargeFire()
+    {
+        AudioSource.PlayClipAtPoint(m_AudioGunSE, transform.position, m_Volume);
+        // 球のプレハブから新しい球を生成
+        GameObject bullet = Instantiate(m_ChargeBulletPrefab, m_MuzzleTransform.position, m_MuzzleTransform.rotation);
+        GameObject particle = Instantiate(m_ParticleGun, m_MuzzleTransform.position, m_MuzzleTransform.rotation);
+        // 球の Rigidbody コンポーネントを取得
+        Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
+        
+        if (bulletRigidbody != null)
+        {
+            // 球をまっすぐ前に発射する力を加える
+            bulletRigidbody.velocity = m_MuzzleTransform.forward * m_BulletSpeed;
+        }
+    }
     private void Update()
     {
         m_Time += Time.deltaTime;
-        if(m_Time > m_CoolTime&&Input.GetMouseButton(0))
+      //  if (isCharge)
+        //{
+
+            if (m_Time > m_CoolTime && Input.GetMouseButton(0))
+            {
+                Fire();
+                m_Time = 0;
+            }
+        //}
+          /*  if (Input.GetMouseButton(1))
         {
-            Fire();
-            m_Time = 0;
-        }
+            isCharge = false;
+            if (Input.GetMouseButton(0))
+            {
+                m_ChargeCoolTime += Time.deltaTime;
+                if(Input.GetMouseButtonUp(0) && m_ChargeCoolTime> m_ChargeTime)
+                {
+                    ChargeFire();
+                    m_ChargeCoolTime = 0;
+                    isCharge = true;
+                }
+                    
+            }
+        }*/
     }
 }
